1944855947daa417a5dbeb68980f2a7b37035556
Letale-vc <letalerv@gmail.com>
2023-12-28 00:55:29 +0200

    add draw list item find

--- UniqueLootHelperCore.cs
+++ UniqueLootHelperCore.cs
@@ -1,16 +1,29 @@
 ﻿using ExileCore;
 using ExileCore.PoEMemory.Components;
+using ExileCore.PoEMemory.MemoryObjects;
 using System.Collections.Generic;
 using System.Linq;
 using System.IO;
-
+using System.Threading.Tasks;
+using System;
+using ImGuiNET;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ImGuiNET;
+using Newtonsoft.Json;
+using SharpDX;
+using Vector2 = System.Numerics.Vector2;
 namespace UniqueLootHelper
 {
     public class UniqueLootHelperCore : BaseSettingsPlugin<Settings>
     {
         private const string UNIQUESARTWORK_FILE = "UniquesArtworks.txt";
         private HashSet<string> UniquesHashSet;
-        
+        private Dictionary<string, int> itemNamesCount = new Dictionary<string, int>();
         public List<SharpDX.RectangleF> drawingList = new List<SharpDX.RectangleF>();
 
         public override bool Initialise()
@@ -47,29 +60,71 @@ namespace UniqueLootHelper
             {
                 Graphics.DrawFrame(frame, Settings.Color, Settings.FrameThickness);
             }
-        }
 
+            DrawItemCountInfo();
+        }
+        private void DrawItemCountInfo()
+        {
+            if (itemNamesCount.Count == 0) return;
+            var posX = 0;
+            var posY = 0;
+            var hight = itemNamesCount.Count * 45;
+            var rect = new RectangleF(posX, posY, 230, hight);
+            Graphics.DrawBox(rect, new Color(0, 0, 0, 200));
+            Graphics.DrawFrame(rect, Color.White, 2);
+
+            posX += 10;
+            posY += 10;
+            foreach (var itemNameCount in itemNamesCount)
+            {
+                Graphics.DrawText($"{itemNameCount.Key}: {itemNameCount.Value}", new Vector2(posX, posY), Color.White, 15);
+                posY += 20;
+            }
+        }
         public override Job Tick()
         {
             drawingList.Clear();
+            itemNamesCount.Clear();
             if (GameController.Area.CurrentArea.IsHideout ||
                 GameController.Area.CurrentArea.IsTown)
             {
                 return null;
             }
-            foreach (var label in GameController.IngameState.IngameUi.ItemsOnGroundLabelsVisible)
+
+            var parallelOptions = new ParallelOptions { MaxDegreeOfParallelism = System.Environment.ProcessorCount };
+
+            var worldItems = GameController.IngameState.IngameUi.ItemsOnGroundLabels?.AsParallel().Where(x => x.IsVisible).ToList() ?? [];
+
+            if (worldItems != null)
             {
-                var worlditem = label.ItemOnGround.GetComponent<WorldItem>();
-                if (worlditem == null) continue;
-                if (worlditem.ItemEntity.Type != ExileCore.Shared.Enums.EntityType.Item) continue;
-                var renderitem = worlditem.ItemEntity.GetComponent<RenderItem>();
-                if (renderitem == null) continue;
-                var modelPath = renderitem.ResourcePath;
-                if (modelPath == null) continue;
-                if (UniquesHashSet.Contains(modelPath))
+                Parallel.ForEach(worldItems, parallelOptions, entity =>
                 {
-                    drawingList.Add(label.Label.GetClientRectCache);
-                }
+                    var worldItem = entity.ItemOnGround?.GetComponent<WorldItem>();
+                    if (worldItem == null || worldItem.ItemEntity.Type != ExileCore.Shared.Enums.EntityType.Item)
+                        return;
+
+                    var renderItem = worldItem.ItemEntity.GetComponent<RenderItem>();
+                    if (renderItem == null || renderItem.ResourcePath == null)
+                        return;
+                    var itemName = worldItem.ItemEntity.GetComponent<Base>().Name;
+                    var modelPath = renderItem.ResourcePath;
+
+                    if (UniquesHashSet.Contains(modelPath))
+                    {
+                        if (itemName != null)
+                        {
+                            if (itemNamesCount.ContainsKey(itemName))
+                            {
+                                itemNamesCount[itemName]++;
+                            }
+                            else
+                            {
+                                itemNamesCount[itemName] = 1;
+                            }
+                        }
+                        drawingList.Add(entity.Label.GetClientRectCache);
+                    }
+                });
             }
             return null;
         }
